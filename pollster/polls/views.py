5b7ebfb2f5018cf22d45093e400fdd34ab939eeb
Kodeck James <kodeckjames095@gmail.com>
2024-07-07 19:48:05 +0300

    Fifth commit

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -3,4 +3,5 @@ from .models import Questions, Choices
 
 # Get questions and display them
 def index(request):
+    latext_question_list = Questions.objects.order_by('-pub_date')[:5]
     return render(request, 'polls/index.html')
