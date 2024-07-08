36a5f1686efbea8a605ba757d5d9974d58bad449
Kodeck James <kodeckjames095@gmail.com>
2024-07-08 19:48:48 +0300

    Sixth commit

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
 from .models import Questions, Choices
+
 
 # Get questions and display them
 def index(request):
-    latext_question_list = Questions.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html')
+    latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# Show specific question and choices
+def detail(request, question_id):
+    try:
+        question = Questions.objects.get(pk=question_id)
+    except Questions.DoesNotExist:
+        raise Http404("Question doesn't exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+# Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+# Vote for a question choice
+def vote(request, question_id):
+    # print(request.POST['choice'])
+    question = get_object_or_404(Questions, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choices.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
--- pollster/pollster/urls.py
+++ pollster/pollster/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-
+   
 ]
